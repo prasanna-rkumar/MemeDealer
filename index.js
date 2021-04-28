6cf41701cbed25634c9c549fb079b3e23e983708
Prasanna Kumar <prasannasrk07@gmail.com>
2021-04-29 00:19:17 +0530

    test image download

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+const fs = require('fs');
+const https = require('https');
+
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
@@ -11,15 +14,20 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
   if (msg.content === 'ping') {
-    msg.reply('pong');
-    // msg.channel.send('pong');
+    https.get("https://preview.redd.it/moa6icoexwv61.jpg?width=640&crop=smart&auto=webp&s=17eaebc955116881dc8b13bc23e023cd96d53bb1", (resp) => {
+      const path = `${__dirname}/assets/moa6icoexwv61.jpg`;
+      const filePath = fs.createWriteStream(path);
+      resp.pipe(filePath);
+      filePath.on('finish', () => {
+        filePath.close();
+        console.log('Download Completed');
+        msg.channel.send("Testing message.", {
+          files: [
+            "./assets/moa6icoexwv61.jpg",
+          ]
+        });
+      })
+    })
 
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
   }
 });
