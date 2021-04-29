87b0b3dca94ed19b39c35907f0ec4d37f81c8e2f
Prasanna Kumar <prasannasrk07@gmail.com>
2021-04-29 23:42:47 +0530

    delete config

--- PostMemesWithInterval.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  execute: () => {
-    setInterval(() => {
-      
-    }, 1000);
-  }
-}
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const { prefix } = require('./config.json');
 const botCommands = require('./BotCommands');
 const { getCommandAndArguments } = require('./utils/getCommandAndArguments');
 
-const bot = new Discord.Client();
+const client = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+const PREFIX = "!";
 
-bot.login(TOKEN);
+client.login(TOKEN);
 
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-bot.on('message', msg => {
-  if (msg.content.startsWith(prefix)) {
-    const { command, args } = getCommandAndArguments(msg.content.toString().replace(prefix, ""));
+client.on('message', msg => {
+  if (msg.content.startsWith(PREFIX)) {
+    const { command, args } = getCommandAndArguments(msg.content.toString().replace(PREFIX, ""));
     switch (command) {
       case "meme":
         botCommands.meme(msg, args);
