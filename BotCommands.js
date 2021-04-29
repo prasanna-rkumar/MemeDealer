403fe0a9aaa62c9b4fbce7f97ecce624001b1122
Prasanna Kumar <prasannasrk07@gmail.com>
2021-04-29 13:10:17 +0530

    fetch random post from a range of subreddits

--- BotCommands.js
+++ BotCommands.js
@@ -1,24 +1,23 @@
-const { downloadFile } = require("./utils/downloadFile");
 const { fetchSubRedditPosts } = require("./utils/fetchSubredditPosts");
 const { randomRange } = require("./utils/randomRange");
 
 module.exports = {
   meme: (msg, args) => {
     fetchSubRedditPosts().then((posts) => {
-      const randomInt = randomRange(0, posts.length)
-      const { title, url } = posts[randomInt].data
-
-      downloadFile(url, (filepath) => {
-        console.log(filepath)
-        msg.channel.send(title, {
-          files: [
-            `${filepath}`
-          ]
-        });
-      })
+      const { title, url, subreddit_name_prefixed: subreddit } = getRandomPost(posts)
+      msg.channel.send(title + "\n" + url + "\n" + "from " + subreddit)
     }).catch((e) => {
       console.log(e)
       msg.channel.send("Unable to download memes")
     })
   }
 };
+
+function getRandomPost(posts) {
+  const randomInt = randomRange(0, posts.length - 1)
+  const post = posts[randomInt].data
+  if (post.is_video) {
+    return getRandomPost(posts)
+  }
+  return post;
+}
--- utils/fetchSubredditPosts.js
+++ utils/fetchSubredditPosts.js
@@ -1,13 +1,22 @@
 const axios = require('axios');
+const { randomRange } = require('./randomRange');
+const SUB_REDDITS = require('../constants/SUB_REDDITS.json')
 
 module.exports = {
   fetchSubRedditPosts: () => {
+    const subreddit = getRandomSubreddit();
     return new Promise((resolve, reject) => {
-      axios.get("https://www.reddit.com/r/ProgrammerHumor/top.json").then((resp) => {
+      axios.get(`https://www.reddit.com/${subreddit}/top.json`).then((resp) => {
+        console.log(subreddit)
         resolve(resp.data.data.children)
       }).catch((err) => {
         reject(err)
       })
     })
   }
+}
+
+function getRandomSubreddit() {
+  const randomInt = randomRange(0, SUB_REDDITS.length - 1)
+  return SUB_REDDITS[randomInt];
 }
